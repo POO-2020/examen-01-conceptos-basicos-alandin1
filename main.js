3523838d0d6ca4f128df0f1e93c30bc6b1f21ce3
alandin1 <alandin1@ucol.mx>
2020-02-26 18:16:48 -0600

    final

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 import Cantidad from "./cantidad.js"
 import Ingrediente from "./ingrediente.js"
+import Receta from "./receta.js"
 
 class Main {
     constructor(){
-
+        this.cantidad1 = new Cantidad(500, "gramos")
+        this.cantidad2 = new Cantidad(300, "gramos")
+        this.ingrediente = new Ingrediente(this.cantidad1, "frijoles", 23)
+        this.ingrediente2 = new Ingrediente(this.cantidad2, "bolillo", 6)
+        this.receta = new Receta("Torta de frijoles", "María López")
     }
 
     pruebaCantidad(){
-        let cantidad = new Cantidad(120, "gramos")
+        let cantidad = new Cantidad(500, "gramos")
         console.log(cantidad.getDescripcion())
     }
 
     pruebaIngrediente(){
-        let ingrediente = new Ingrediente(new Cantidad(10, "gramos"), "de harina", 13)
-        console.log(ingrediente.getDescripcion())
+        let ingrediente1 = new Ingrediente(new Cantidad(500, "gramos"), "de frijoles", 23)
+        let ingrediente2 = new Ingrediente(new Cantidad(300, "gramos"), "bolillo", 6)
+        console.log(ingrediente1.getDescripcion())
+        console.log(ingrediente2.getDescripcion())
     }
 
+    pruebaReceta(){
+        this.receta = new Receta("Torta de frijoles.", "María López.")
+        this.receta.agregarIngrediente(this.ingrediente)
+        this.receta.agregarIngrediente(this.ingrediente2)
+        this.receta.imprimirEnConsola()
+    }
 }
 
 let app = new Main
 
 app.pruebaCantidad()
 app.pruebaIngrediente()
+app.pruebaReceta()
--- receta.js
+++ receta.js
@@ -1,16 +1,37 @@
-import Ingrediente from "./ingrediente.js"
-
-export default class Receta {
+export default class Receta{
     /**
-     * 
-     * @param {string} nombre 
-     * @param {string} autor 
-     * @param {Ingrediente[]} ingredientes 
+     * @param {string} nombre de la receta
+     * @param {string} autor de la receta
      */
-    constructor(nombre, autor, ingredientes){
+    constructor(nombre, autor){
         this.nombre = nombre
         this.autor = autor
-        this.ingredientes = ingredientes
+        this.ingredientes = new Array
     }
-    
+
+    agregarIngrediente(ingrediente){
+        this.ingredientes.push(ingrediente)
+    }
+
+    getNumeroIngredientes(){
+        let numero = 0
+        this.ingredientes.forEach((ingrediente, i) => {
+            numero = i})
+        return numero+1
+    }
+
+    getCosto(){
+        let total=0
+        this.ingredientes.forEach((ingrediente, i) => {
+            total = (total + ingrediente.costo)})
+        return total
+    }
+
+    imprimirEnConsola(){
+        console.log(`${this.nombre} / Autor: ${this.autor} / Ingredientes:`)
+        this.ingredientes.forEach((ingrediente, i) => {
+             console.log(`${i+1}. ${ingrediente.getDescripcion()}`)
+        })
+        console.log(`Costo total $${this.getCosto()} pesos.`)
+    }
 }
